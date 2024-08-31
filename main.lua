ef18798caec9bebf2395a80dd5d86329dc4298e2
Nibbl-z <thisisnotnibblz123@gmail.com>
2024-08-31 13:19:41 -0600

    HE GOES IN THE DOOR !

--- main.lua
+++ main.lua
@@ -7,8 +7,9 @@ local stairXOffset, stairYOffset = 15, 0
 require "yan"
 require "biribiri"
 
-local playyan = {X = 121, Y = 58, Sprite = "img/yan_stand.png", Moving = false, Direction = 1}
+local playyan = {X = 121, Y = 58, Sprite = "img/yan_stand.png", Moving = false, Direction = 1, Visible = true}
 local jumpSpeed = 0.15
+local currentMedia = 1
 
 local media = {
     {
@@ -16,9 +17,15 @@ local media = {
         files = {
             "song2.mp3",
             "song3.mp3"
-        }
+        },
+        type = "folder",
+        sprite = "img/door.png"
     },
-    "song1.mp3"
+    {
+        name = "song.mp3",
+        type = "file",
+        sprite = "img/music.png"
+    }
 }
 
 function love.load()
@@ -35,9 +42,22 @@ function love.load()
     yanUpTimer = biribiri:CreateTimer(jumpSpeed / 2, function ()
         yan:NewTween(playyan, yan:TweenInfo(jumpSpeed / 2, EasingStyle.QuadOut), {Y = playyan.Y + 30 - stairHeight}):Play()
     end)
-
+    
     yanDownTimer = biribiri:CreateTimer(jumpSpeed / 2, function ()
         yan:NewTween(playyan, yan:TweenInfo(jumpSpeed / 2, EasingStyle.QuadOut), {Y = playyan.Y + 10 + stairHeight}):Play()
+    end)
+    
+    doorEnterTimer = biribiri:CreateTimer(0.2, function ()
+        media[currentMedia].sprite = "img/door_whoosh.png"
+        playyan.Visible = false
+    end)
+
+    doorUnwhooshTimer = biribiri:CreateTimer(0.35, function ()
+        media[currentMedia].sprite = "img/door_open.png"
+    end)
+    
+    doorCloseTimer = biribiri:CreateTimer(0.45, function ()
+        media[currentMedia].sprite = "img/door.png"
     end)
 end
 
@@ -45,6 +65,9 @@ function love.keypressed(key)
 
     if key == "a" then
         if playyan.Moving then return end
+
+        currentMedia = currentMedia - 1
+
         playyan.Moving = true
         yan:NewTween(camera, yan:TweenInfo(jumpSpeed / 2), {X = camera.X + stairWidth, Y = camera.Y - stairHeight}):Play()
         playyan.Sprite = "img/yan_jump.png"
@@ -55,9 +78,12 @@ function love.keypressed(key)
         yanDownTimer:Start()
 
         playyan.Direction = -1
-
+    
     elseif key == "d" then
         if playyan.Moving then return end
+
+        currentMedia = currentMedia + 1
+
         playyan.Moving = true
         yan:NewTween(camera, yan:TweenInfo(jumpSpeed / 2), {X = camera.X - stairWidth, Y = camera.Y + stairHeight}):Play()
         playyan.Sprite = "img/yan_jump.png"
@@ -68,6 +94,17 @@ function love.keypressed(key)
         yanUpTimer:Start()
 
         playyan.Direction = 1
+    elseif key == "space" then
+        if playyan.Moving then return end
+
+        if media[currentMedia].type == "folder" then
+            playyan.Direction = 1
+            playyan.Moving = true
+            media[currentMedia].sprite = "img/door_open.png"
+            doorEnterTimer:Start()
+            doorUnwhooshTimer:Start()
+            doorCloseTimer:Start()
+        end
     end
 end
 
@@ -82,7 +119,7 @@ function love.draw()
     love.graphics.translate(camera.X, camera.Y)
     love.graphics.setColor(0,1,1)
     
-    for i = 1, 100 do
+    for i = -10, 100 do
         love.graphics.line(
             stairXOffset + ((i - 1) * stairWidth), 
             height - ((i - 1) * stairHeight), 
@@ -98,15 +135,15 @@ function love.draw()
     end
     love.graphics.setColor(1,1,1)
     for i, file in ipairs(media) do
-        local sprite = (file.name == nil) and "img/music.png" or "img/door.png"
+        love.graphics.draw(assets[file.sprite], 15 + ((i + 3) * stairWidth), height - ((i + 3) * stairHeight) - 39)
         
-        love.graphics.draw(assets[sprite], 15 + ((i - 1) * stairWidth), height - ((i - 1) * stairHeight) - 39)
-        
-        love.graphics.print(file.name or file, 15 + ((i - 1) * stairWidth) + stairWidth + 5, height - ((i - 1) * stairHeight) - 33)
+        love.graphics.print(file.name, 15 + ((i + 3) * stairWidth) + stairWidth + 5, height - ((i + 3) * stairHeight) - 33)
     end
     
+    if playyan.Visible then
+        love.graphics.draw(assets[playyan.Sprite], playyan.X + 6, playyan.Y + 8, 0, playyan.Direction, 1, 6, 9)
+    end
     
-    love.graphics.draw(assets[playyan.Sprite], playyan.X + 6, playyan.Y + 8, 0, playyan.Direction, 1, 6, 9)
     
     love.graphics.pop()
 end
