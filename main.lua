75d09907a859b08fb5fcf9b8d7cb01fc2549834a
Nibbl-z <thisisnotnibblz123@gmail.com>
2024-08-31 14:06:38 -0600

    the yan jumping out of the door !!

--- main.lua
+++ main.lua
@@ -8,9 +8,11 @@ require "yan"
 require "biribiri"
 
 local playyan = {X = 121, Y = 58, Sprite = "img/yan_stand.png", Moving = false, Direction = 1, Visible = true}
-local backdoor = {X = 100, Y = 100, Index = 1}
+local backdoor = {X = 121 - 50, Y = 58 - 50, Index = 1, Visible = false}
 local jumpSpeed = 0.15
 local currentMedia = 1
+
+local loading = false
 
 local media = {
     {
@@ -41,12 +43,12 @@ function love.load()
         playyan.Moving = false
     end)
     
-    yanUpTimer = biribiri:CreateTimer(jumpSpeed / 2, function ()
-        yan:NewTween(playyan, yan:TweenInfo(jumpSpeed / 2, EasingStyle.QuadOut), {Y = playyan.Y + 30 - stairHeight}):Play()
+    yanUpTimer = biribiri:CreateTimer(jumpSpeed / 1.9, function ()
+        yan:NewTween(playyan, yan:TweenInfo(jumpSpeed / 2.2, EasingStyle.QuadOut), {Y = playyan.Y + 30 - stairHeight}):Play()
     end)
     
-    yanDownTimer = biribiri:CreateTimer(jumpSpeed / 2, function ()
-        yan:NewTween(playyan, yan:TweenInfo(jumpSpeed / 2, EasingStyle.QuadOut), {Y = playyan.Y + 10 + stairHeight}):Play()
+    yanDownTimer = biribiri:CreateTimer(jumpSpeed / 1.9, function ()
+        yan:NewTween(playyan, yan:TweenInfo(jumpSpeed / 2.2, EasingStyle.QuadOut), {Y = playyan.Y + 10 + stairHeight}):Play()
     end)
     
     doorEnterTimer = biribiri:CreateTimer(0.2, function ()
@@ -79,6 +81,7 @@ function love.keypressed(key)
 
         playyan.Moving = true
         yan:NewTween(camera, yan:TweenInfo(jumpSpeed / 2), {X = camera.X + stairWidth, Y = camera.Y - stairHeight}):Play()
+        yan:NewTween(backdoor, yan:TweenInfo(jumpSpeed, EasingStyle.QuadInOut), {X = backdoor.X - stairWidth, Y = backdoor.Y + stairHeight}):Play()
         playyan.Sprite = "img/yan_jump.png"
         unjumpTimer:Start()
         
@@ -90,22 +93,25 @@ function love.keypressed(key)
     
     elseif key == "d" then
         if playyan.Moving then return end
-
+        
         currentMedia = currentMedia + 1
 
         playyan.Moving = true
         yan:NewTween(camera, yan:TweenInfo(jumpSpeed / 2), {X = camera.X - stairWidth, Y = camera.Y + stairHeight}):Play()
+        yan:NewTween(backdoor, yan:TweenInfo(jumpSpeed, EasingStyle.QuadInOut), {X = backdoor.X + stairWidth, Y = backdoor.Y - stairHeight}):Play()
+
         playyan.Sprite = "img/yan_jump.png"
         unjumpTimer:Start()
         
         yan:NewTween(playyan, yan:TweenInfo(jumpSpeed / 2, EasingStyle.Linear), {X = playyan.X + stairWidth}):Play()
         yan:NewTween(playyan, yan:TweenInfo(jumpSpeed / 2, EasingStyle.QuadOut), {Y = playyan.Y - 30}):Play()
+        
         yanUpTimer:Start()
 
         playyan.Direction = 1
     elseif key == "space" then
         if playyan.Moving then return end
-
+        
         if media[currentMedia].type == "folder" then
             playyan.Direction = 1
             playyan.Moving = true
@@ -113,6 +119,32 @@ function love.keypressed(key)
             doorEnterTimer:Start()
             doorUnwhooshTimer:Start()
             doorCloseTimer:Start()
+
+            biribiri:CreateAndStartTimer(1, function ()
+                fading = true
+                backdoor.Visible = true
+                biribiri:CreateAndStartTimer(0.5, function()
+                    fading = false
+                    playyan.Visible = true
+                    playyan.X = backdoor.X + 20
+                    playyan.Y = backdoor.Y + 22
+                    
+                    biribiri:CreateAndStartTimer(0.7, function ()
+                        yan:NewTween(playyan, yan:TweenInfo(jumpSpeed / 0.5, EasingStyle.Linear), {X = playyan.X + stairWidth + 6}):Play()
+                        yan:NewTween(playyan, yan:TweenInfo(jumpSpeed, EasingStyle.QuadOut), {Y = playyan.Y - 20}):Play()
+                        playyan.Sprite = "img/yan_jump.png"
+                        biribiri:CreateAndStartTimer(jumpSpeed, function ()
+                            yan:NewTween(playyan, yan:TweenInfo(jumpSpeed, EasingStyle.QuadIn), {Y = playyan.Y + 48}):Play()
+                        end)
+                        
+                        biribiri:CreateAndStartTimer(jumpSpeed / 0.5, function ()
+                            playyan.Sprite = "img/yan_stand.png"
+                            playyan.Moving = false
+                        end)
+                    end)
+                end)
+            end)
+        
         end
     end
 end
@@ -127,6 +159,11 @@ function love.draw()
     love.graphics.scale(3, 3)
     love.graphics.translate(camera.X, camera.Y)
     love.graphics.setColor(0,1,1)
+    
+    if fading then
+        love.graphics.pop()
+        return
+    end
     
     for i = -10, 100 do
         love.graphics.line(
@@ -148,12 +185,15 @@ function love.draw()
         
         love.graphics.print(file.name, 15 + ((i + 3) * stairWidth) + stairWidth + 5, height - ((i + 3) * stairHeight) - 33)
     end
-    
+    if backdoor.Visible then
+        love.graphics.draw(assets["img/backdoor_"..tostring(backdoor.Index)..".png"], backdoor.X + 6, backdoor.Y + 8)
+    end
     if playyan.Visible then
         love.graphics.draw(assets[playyan.Sprite], playyan.X + 6, playyan.Y + 8, 0, playyan.Direction, 1, 6, 9)
     end
     
-    love.graphics.draw(assets["img/backdoor_"..tostring(backdoor.Index)..".png"], backdoor.X + 6, backdoor.Y + 8)
+    
+    
     
     love.graphics.pop()
 end
